599e8c8d9bb17d7c8be3932a355e25228aa9cef7
Amit Kumar <amitkumar.php@gmail.com>
2020-06-06 01:55:01 +0530

    Create file store.json locally if not exist

--- mod.ts
+++ mod.ts
@@ -1,9 +1,12 @@
+import * as path from "https://deno.land/std/path/mod.ts";
 import { readFileStr } from "https://deno.land/std/fs/read_file_str.ts";
 import { writeFileStr } from "https://deno.land/std/fs/write_file_str.ts";
+import { exists } from "https://deno.land/std/fs/exists.ts";
 
 import { clipboard } from "./clipboard.ts";
 
-const jsonFile = "./store.json";
+const homeDir: string = (Deno.env.get("HOME")) || "";
+const jsonFile = path.join(homeDir, "./store.json");
 
 type AliasText = {
   alias: string;
@@ -13,6 +16,13 @@ type AliasText = {
 /** Read/Write text for an alias and copy it to clipboard */
 export async function a0(alias: string, text: string) {
   let result: string = "";
+
+  if (!await exists(jsonFile)) {
+    await writeFileStr(jsonFile, "[]");
+    // make sure file have been create.
+    await Deno.stat(jsonFile);
+  }
+
   if (alias) {
     const strFile = await readFileStr(jsonFile);
     let store: AliasText[] = JSON.parse(strFile);
