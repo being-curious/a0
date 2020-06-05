0e4a256473e561655d8b68f9c7d201dcbbd1c06a
Amit Kumar <amitkumar.php@gmail.com>
2020-06-05 15:39:35 +0530

    Allow one space between words for second arugument 'text'

--- cli.ts
+++ cli.ts
@@ -3,7 +3,7 @@ import { a0 } from "./mod.ts";
 async function cli() {
   if (Deno.args.length) {
     const alias = Deno.args[0];
-    const text = Deno.args[1];
+    const text = Deno.args.slice(1).join(" ");
     const result = await a0(alias, text);
     console.log("alias: %s", alias);
     console.log("text: %s", result);
